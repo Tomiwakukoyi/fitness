be1ecf4cf399a71824ccf7660623e19ee8b36126
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-05-21 11:05:51 +0100

    benefits update

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -4,6 +4,8 @@ import { motion } from "framer-motion";
 import HText from "@/shared/HText";
 import { BenefitType } from "@/shared/types";
 import Benefit from "./Benefit";
+import ActionButton from "@/shared/ActionButton";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png"
 
 const benefits:Array<BenefitType> = [
   {
@@ -70,6 +72,49 @@ const Benefits = ({setSelectedPage}: Props) => {
             />
           ))}
         </motion.div>
+        {/* Graphis and desc */}
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* graphic img */}
+          <img className="mx-auto" src={BenefitsPageGraphic} alt="benefits-graphic" />
+          {/* description */}
+          <div className="relativ">
+            {/* title */}
+            <motion.div initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: 50 },
+            visible: { opacity: 1, x: 0 }
+          }}>
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
+                <div>
+                  <HText>Lorem ipsum dolor sit amet.
+                  <span className="text-primary-500">FIT</span></HText>
+                </div>
+              </div>
+            </motion.div>
+            {/* desc */}
+
+            <motion.div initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay:0.2,duration: 0.7 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 }
+          }}>
+              <p className="my-5 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, tempora. Recusandae sit, aperiam dolorum voluptates assumenda eveniet illum vel dicta!</p>
+              <p className=" mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus nemo sequi illo odit odio adipisci fuga ea eos aperiam iure?</p>
+            </motion.div>
+            {/* button */}
+            <div className=" relative mt-16">
+              <div className=" before:absolute before:-bottom-20 before:right-40 before:z-[1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
+          </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   )
